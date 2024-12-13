e2259f6ef065b13dd48b215b348055dc571bf111
Wesley Silva <wesleysilva059@gmail.com>
2024-12-13 00:00:42 -0300

    fix: fix route

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Rotas acessíveis a qualquer usuário
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-
-// Rotas protegidas por autenticação
-Route::middleware('auth')->group(function () {
-    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-    Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+Route::prefix('contacts')->group(function(){
+    Route::get('/',  [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('edit/{contact}',  [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('store', [ContactController::class, 'store'])->name('contacts.store');
+    Route::put('update/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+    Route::delete('delete/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+    Route::get('show/{contact}', [ContactController::class, 'show'])->name('contacts.show');
 });
